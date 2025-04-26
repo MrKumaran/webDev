44e706e2e99d47c45055758751c6f05d0eb4d2c6
Kumaran S <kumaran.s@velammal.edu.in>
2025-04-26 18:25:28 +0530

    added color

--- js scribme/score counter/index.js
+++ js scribme/score counter/index.js
@@ -7,33 +7,55 @@ let guestScoreCount = 0
 homeScore.innerText = homeScoreCount
 guestScore.innerText = guestScoreCount
 
+function color(){
+    if (homeScoreCount > guestScoreCount) {
+        homeScore.style.color = "green"
+        guestScore.style.color = "red"
+    }
+    else if (homeScoreCount < guestScoreCount){
+        guestScore.style.color = "green"
+        homeScore.style.color = "red"
+    }
+    else{
+        guestScore.style.color = "#F94F6D"
+        homeScore.style.color = "#F94F6D"
+    }
+}
+
 function incrementOneHome(){
     homeScoreCount += 1
     homeScore.innerText = homeScoreCount
+    color()
 }
 
 function incrementOneGuest(){
     guestScoreCount += 1
     guestScore.innerText = guestScoreCount
+    color()
 }
 
 function incrementTwoHome(){
     homeScoreCount += 2
     homeScore.innerText = homeScoreCount
+    color()
 }
 
 function incrementTwoGuest(){
     guestScoreCount += 2
-    guestScore.innerText = guestScoreCount}
+    guestScore.innerText = guestScoreCount
+    color()
+}
 
 function incrementThreeHome(){
     homeScoreCount += 3
     homeScore.innerText = homeScoreCount
+    color()
 }
 
 function incrementThreeGuest(){
     guestScoreCount += 3
     guestScore.innerText = guestScoreCount
+    color()
 }
 
 function resetScore(){
@@ -41,4 +63,5 @@ function resetScore(){
     guestScoreCount = 0
     homeScore.innerText = homeScoreCount
     guestScore.innerText = guestScoreCount
+    color()
 }
