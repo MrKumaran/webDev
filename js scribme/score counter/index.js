21735d6ee57d8174764ba288d87538876fcaf95e
Kumaran S <kumaran.s@velammal.edu.in>
2025-04-26 19:55:16 +0530

    confetti

--- js scribme/score counter/index.js
+++ js scribme/score counter/index.js
@@ -1,8 +1,10 @@
 let homeScore = document.getElementById("home-score")
 let guestScore = document.getElementById("guest-score")
+let winner = document.getElementById("who-wins")
 
 let homeScoreCount = 0
 let guestScoreCount = 0
+let wins = false
 
 homeScore.innerText = homeScoreCount
 guestScore.innerText = guestScoreCount
@@ -20,42 +22,55 @@ function color(){
         guestScore.style.color = "#F94F6D"
         homeScore.style.color = "#F94F6D"
     }
+    whoWins()
 }
 
 function incrementOneHome(){
-    homeScoreCount += 1
-    homeScore.innerText = homeScoreCount
-    color()
+    if(!wins){
+        homeScoreCount += 1
+        homeScore.innerText = homeScoreCount
+        color()
+    }
 }
 
 function incrementOneGuest(){
+    if(!wins){
     guestScoreCount += 1
     guestScore.innerText = guestScoreCount
     color()
+    }
 }
 
 function incrementTwoHome(){
+    if(!wins){
     homeScoreCount += 2
     homeScore.innerText = homeScoreCount
     color()
+    }
 }
 
 function incrementTwoGuest(){
+    if(!wins){
     guestScoreCount += 2
     guestScore.innerText = guestScoreCount
     color()
+    }
 }
 
 function incrementThreeHome(){
+    if(!wins){
     homeScoreCount += 3
     homeScore.innerText = homeScoreCount
     color()
+    }
 }
 
 function incrementThreeGuest(){
+    if(!wins){
     guestScoreCount += 3
     guestScore.innerText = guestScoreCount
     color()
+    }
 }
 
 function resetScore(){
@@ -64,4 +79,44 @@ function resetScore(){
     homeScore.innerText = homeScoreCount
     guestScore.innerText = guestScoreCount
     color()
+    wins = false
+}
+
+function whoWins(){
+    if (homeScoreCount > 25 || guestScoreCount > 25) {
+        if (homeScoreCount > guestScoreCount + 1) {
+            winner.innerText = "Home"
+            winner.style.display = 'block'
+            celeb()
+            wins = !wins
+        } else if(homeScoreCount + 1 < guestScoreCount) {
+            winner.innerText = "Guest"
+            winner.style.display = 'block'
+            celeb()
+            wins = !wins
+        }
+    }
+}
+
+function celeb(){
+    var duration = 30 * 1000;
+    var end = Date.now() + duration;
+
+    (function frame() {
+    confetti({
+        particleCount: 7,
+        angle: 60,
+        spread: 100,
+        origin: { x: 0 }
+    });
+    confetti({
+        particleCount: 7,
+        angle: 120,
+        spread: 100,
+        origin: { x: 1 }
+    });
+    if (Date.now() < end) {
+        requestAnimationFrame(frame);
+    }
+    }());
 }
